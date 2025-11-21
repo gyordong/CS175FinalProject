60b9b489d76a21c4dce7edcea2461f3ad40dc9d6
Ryan Duong <ryan.duong02@sjsu.edu>
2025-11-20 19:17:30 -0800

    bug fixes

--- app/src/main/java/edu/sjsu/android/cs175finalproject/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/cs175finalproject/MainActivity.java
@@ -7,18 +7,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        FragmentContainerView navHostFragment = findViewById(R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        navController.setGraph(R.navigation.nav);
+
+//        if (true) {
+//            // Load main app flow
+//            navController.setGraph(R.navigation.nav);
+//        } else {
+//            // Load auth flow
+//            navController.setGraph(R.navigation.nav_auth);
+//        }
     }
 }
