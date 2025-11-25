3bdce6c35322c6100500713f7461466eb041a73e
Ryan Duong <ryan.duong02@sjsu.edu>
2025-11-25 04:41:41 -0800

    fixed camera crash bug

--- app/src/main/java/edu/sjsu/android/cs175finalproject/CameraFragment.java
+++ app/src/main/java/edu/sjsu/android/cs175finalproject/CameraFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,6 +18,7 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
@@ -43,7 +43,7 @@ public class CameraFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         previewView = view.findViewById(R.id.previewView);
-        Button captureButton = view.findViewById(R.id.captureButton);
+        FloatingActionButton captureButton = view.findViewById(R.id.captureButton);
 
         if (allPermissionsGranted()) {
             startCamera();
