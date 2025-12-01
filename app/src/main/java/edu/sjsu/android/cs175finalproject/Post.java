fc31277356fc1ad5cd4b3a198154f42d08ad6279
Rachel <racheltsai03@gmail.com>
2025-11-30 22:18:38 -0800

    home page changed to support dynamic data, added react btn

---
+++ app/src/main/java/edu/sjsu/android/cs175finalproject/Post.java
@@ -0,0 +1,5 @@
+package edu.sjsu.android.cs175finalproject;
+
+public class Post {
+
+}
---
+++ app/src/main/java/edu/sjsu/android/cs175finalproject/PostAdapter.java
@@ -0,0 +1,13 @@
+package edu.sjsu.android.cs175finalproject;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostAdapter extends RecyclerView {
+
+    public PostAdapter(@NonNull Context context) {
+        super(context);
+    }
+}
